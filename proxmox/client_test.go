7e3b648731797eacbf71a2d857379f2cb5678236
jle35 <addr.poub@gmail.com>
2020-08-03 14:31:16 +1100

    chore(yaml): golint and pxe creds

--- proxmox/client_test.go
+++ proxmox/client_test.go
@@ -11,6 +11,6 @@ func TestClient_Login(t *testing.T) {
 	client, err := NewClient("https://localhost:8006/api2/json", nil, &tls.Config{InsecureSkipVerify: true}, 300)
 	assert.Nil(t, err)
 
-	err = client.Login("root@pam", "vagrant", "")
+	err = client.Login("root@pam", "root", "")
 	assert.Nil(t, err)
 }
